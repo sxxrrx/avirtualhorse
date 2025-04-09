7e7cc226c598905ac4c6470939699dc9fd0cb536
sxxrrx <horsegamebysxxrrx@gmail.com>
2025-04-08 20:44:43 -0400

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,19 @@
+// firebase-init.js
+// Firebase core + modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkFOc0BwRqmR2LkjHj0vwXSAS1h4BlBCE",
+  authDomain: "horse-game-by-sxxrrx.firebaseapp.com",
+  projectId: "horse-game-by-sxxrrx",
+  storageBucket: "horse-game-by-sxxrrx.firebasestorage.app",
+  messagingSenderId: "87883054918",
+  appId: "1:87883054918:web:4771a90eb5c6a3e7c0ef47",
+  measurementId: "G-ZW6W5HVXBJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
