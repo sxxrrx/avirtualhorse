3c21d6fad9d9a91a287fe223082e9cc346966309
sxxrrx <horsegamebysxxrrx@gmail.com>
2025-08-06 22:49:52 -0400

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,17 @@
-// firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAuth }        from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-import { getDatabase }    from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js';
+import { getAuth }        from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js';
+import { getDatabase }    from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js';
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",        // ← add this if you haven’t yet
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
-
 const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db   = getDatabase(app);
-
 export { auth, db };
